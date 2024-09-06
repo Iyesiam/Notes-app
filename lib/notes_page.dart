c3eb8e4348d97dd36abdff6e788ea60d5ac067df
Iyesiam <zawahwi10@gmail.com>
2024-09-06 18:14:26 +0200

    updated the ui

--- lib/notes_page.dart
+++ lib/notes_page.dart
@@ -1,46 +1,54 @@
-import'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:noteapp/model/note.dart';
 import 'package:noteapp/notes_database.dart';
 import 'package:provider/provider.dart';
 
-class NotesPage  extends StatefulWidget {
-  const NotesPage ({super.key});
+class NotesPage extends StatefulWidget {
+  const NotesPage({super.key});
 
   @override
-  State<NotesPage > createState() => _NotesPageState();
+  State<NotesPage> createState() => _NotesPageState();
 }
 
-class _NotesPageState extends State<NotesPage > {
-final textController =TextEditingController();
+class _NotesPageState extends State<NotesPage> {
+  final textController = TextEditingController();
 
-@override
-void initState(){
-  super.initState();
+  @override
+  void initState() {
+    super.initState();
+    readNotes();
+  }
 
-  readNotes();
-}
   void createNote() {
     showDialog(
-      context: context, 
+      context: context,
       builder: (context) => AlertDialog(
         content: TextField(
           controller: textController,
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.grey[200],
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide.none,
+            ),
+          ),
         ),
         actions: [
-          MaterialButton(
+          neumorphicButton(
             onPressed: () {
-
               context.read<NoteDatabase>().addnote(textController.text);
-
               textController.clear();
               Navigator.pop(context);
-            }, 
-            child: const Text("Create"),
-            )
+            },
+            text: "Create",
+          ),
         ],
-      )
-      );
-      
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+    );
   }
 
   void readNotes() {
@@ -48,66 +56,114 @@ void initState(){
   }
 
   void updateNotes(Note note) {
-
     textController.text = note.text;
     showDialog(
-      context: context, 
+      context: context,
       builder: (context) => AlertDialog(
-        content: TextField(controller:textController),
+        content: TextField(
+          controller: textController,
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.grey[200],
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide.none,
+            ),
+          ),
+        ),
         actions: [
-          MaterialButton(
-            onPressed: (){
-              context
-              .read<NoteDatabase>()
-              .updateNotes(note.id, textController.text);
+          neumorphicButton(
+            onPressed: () {
+              context.read<NoteDatabase>().updateNotes(note.id, textController.text);
               textController.clear();
               Navigator.pop(context);
             },
-            child: const Text("update"),
-            )
+            text: "Update",
+          ),
         ],
-      )
-      );
-
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+    );
   }
 
   void deleteNote(int id) {
     context.read<NoteDatabase>().deleteNotes(id);
   }
+
   @override
   Widget build(BuildContext context) {
     final noteDatabase = context.watch<NoteDatabase>();
+    List<Note> currentNotes = noteDatabase.currentNotes;
 
-    List<Note> currentNotes = noteDatabase.currentNotes;    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(title: const Text('Notes')),
       floatingActionButton: FloatingActionButton(
         onPressed: createNote,
         child: const Icon(Icons.add),
-        ),
-        body: ListView.builder(
-          itemCount: currentNotes.length,
-          itemBuilder: (context, index) {
-            final note = currentNotes[index];
-
-            return ListTile(
-              title: Text(note.text),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-
-                   IconButton(
-                    onPressed: () => updateNotes(note), 
-                    icon: const Icon(Icons.edit),
-                    ),
-
+      ),
+      body: ListView.builder(
+        itemCount: currentNotes.length,
+        itemBuilder: (context, index) {
+          final note = currentNotes[index];
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: neumorphicDecoration(),
+              child: ListTile(
+                title: Text(note.text),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
                     IconButton(
-                    onPressed: () => deleteNote(note.id), 
-                    icon: const Icon(Icons.delete),
-                    )
-                ],
+                      onPressed: () => updateNotes(note),
+                      icon: const Icon(Icons.edit),
+                    ),
+                    IconButton(
+                      onPressed: () => deleteNote(note.id),
+                      icon: const Icon(Icons.delete),
+                    ),
+                  ],
+                ),
               ),
-            );
-          }          ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  // Neumorphic button widget
+  Widget neumorphicButton({required VoidCallback onPressed, required String text}) {
+    return Container(
+      decoration: neumorphicDecoration(),
+      child: MaterialButton(
+        onPressed: onPressed,
+        child: Text(text),
+      ),
+    );
+  }
+
+  // Neumorphic decoration function
+  BoxDecoration neumorphicDecoration() {
+    return BoxDecoration(
+      color: Colors.grey[300],
+      borderRadius: BorderRadius.circular(12),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.grey[500]!,
+          offset: Offset(4, 4),
+          blurRadius: 15,
+          spreadRadius: 1,
+        ),
+        BoxShadow(
+          color: Colors.white,
+          offset: Offset(-4, -4),
+          blurRadius: 15,
+          spreadRadius: 1,
+        ),
+      ],
     );
   }
 }
